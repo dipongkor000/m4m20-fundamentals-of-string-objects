05756eb78e935317ccd7537dcadae5343122833e
Dipongkor roy <dipongkorroy000@gmail.com>
2025-02-12 20:53:14 +0600

    Update first5.js

--- first5.js
+++ first5.js
@@ -1,16 +1,16 @@
-//object type: primitive
-//basic, primary
+//object type: primitive --
+//basic, primary --
 const age = 21;
 const school = 'north collage';
 const isPassed = true;
 
-// let isDeveloper;
+// let isDeveloper --
 const allSubjects = ['english','physics','math','chemistry'];
 const bottleDetails = ['white', 45];
 const bottleColor = 'white';
 const bottlePrice = 45;
 
-// object type: non-primitive
+// object type: non-primitive --
 const bottle = {
     brand: 'rfl',
     color: 'white',
@@ -18,7 +18,7 @@ const bottle = {
     price: 45,
     isClean: false
 }
-//object type: non-primitive
+//object type: non-primitive --
 const subjects = {
     name: 'chemistry',
     teacher: 'shapon',
